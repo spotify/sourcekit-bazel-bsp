95915f2272e2bd1228268cd5d7f2431ae9dd208f
Sean Zhang <sean7218@gmail.com>
2025-07-17 08:45:34 -0400

    BazelProtobufBindings for Parsing ActionGraph (#14)
    * Add Protobuf Definitions and Action Parser Tests
    
    - Introduced `analysis_v2.proto` to define Protobuf messages for aquery data, including module information, rule definitions, and provider details.
    - Created a README.md file for the Action Parser, outlining setup instructions and usage for generating Swift bindings from Protobuf definitions.
    - Implemented unit tests in `ActionParserTests.swift` to validate the parsing of action graph data from Protobuf output, including tests for compiler arguments and action targets.
    - Added sample Protobuf data file `actions.pb` for testing purposes.
    
    * Slim down the binding files generated
    
      1. remove build.proto and build.pb.swift
      2. remove stardoc_output.proto and stardoc_output.pb.swift
      3. use Target defined in analysis_v2.proto instead of blaze_query.Target
    
    * updated NOTICE for swift-protobuf
    
    * rename ActionParser to BazelProtobufBindings

---
+++ Sources/BazelProtobufBindings/BazelProtobufBindings.swift
@@ -0,0 +1,42 @@
+// Copyright (c) 2025 Spotify AB.
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import Foundation
+
+public struct BazelProtobufBindings {
+    
+    let actionGraph: Analysis_ActionGraphContainer
+    
+    init(actionGraph: Analysis_ActionGraphContainer) {
+        self.actionGraph = actionGraph
+    }
+
+    public static func new(data: Data) throws -> Self {
+        let actionGraph = try Analysis_ActionGraphContainer(serializedBytes: data)
+        return BazelProtobufBindings(actionGraph: actionGraph)
+    }
+    
+    public func findActionsBy(mnemonic: String) -> [Analysis_Action] {
+        return actionGraph.actions.filter { $0.mnemonic == mnemonic }
+    }
+    
+    public func getTargetBy(label: String) -> Analysis_Target? {
+         return actionGraph.targets.first { $0.label == label }
+    }
+}
