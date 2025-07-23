12a064d71bb37325019cb0b9bf55e52645930900
Bruno Henrique da Rocha e Silva <rochab@spotify.com>
2025-07-23 14:12:20 +0200

    Bump BSP version

--- Sources/SourceKitBazelBSP/RequestHandlers/InitializeHandler.swift
+++ Sources/SourceKitBazelBSP/RequestHandlers/InitializeHandler.swift
@@ -21,7 +21,7 @@ import BuildServerProtocol
 import Foundation
 import LanguageServerProtocol
 
-package let sourcekitBazelBSPVersion = "0.0.1"
+package let sourcekitBazelBSPVersion = "0.0.2"
 private let logger = makeFileLevelBSPLogger()
 
 enum InitializeHandlerError: Error, LocalizedError {
